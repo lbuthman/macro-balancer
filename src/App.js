b6ebd7f2775354196ef896b2f888bb376c9ee1f9
lukebuthman <lbuthman@gmail.com>
2023-12-11 15:28:54 -0600

    implement the macro balancer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-//import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import './KPIs';
 import KPIs from "./KPIs";
 
 function App() {
 
-
     return (
-        <KPIs/>
+        <div className="App">
+            <h1 className="pt-5 text-white">Macro Nutrient Calculator</h1>
+            <KPIs/>
+
+            <hr className="border-top my-3"/>
+            <h1 className="text-white">Caloric Deficit Balancer</h1>
+        </div>
     );
 }
 
--- src/KPIs.js
+++ src/KPIs.js
@@ -1,43 +1,90 @@
-import GaugeChart from 'react-gauge-chart';
-import './KPIs.css';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import {Button} from "react-bootstrap";
+import { useState } from 'react';
 
 function KPIs() {
+    const [proteinPercentage, setProteinPercentage] = useState('0%');
+    const [carbPercentage, setCarbPercentage] = useState('0%');
+    const [fatPercentage, setFatPercentage] = useState('0%');
 
-    const carbRanges = [0.58, 0.15, 0.15, 0.15, 1];
-    const otherRanges = [0.3, 0.15, 0.15, 0.15, 1];
-    const colors = ['#FF0000', '#FFFF00', '#00FF1E', '#FFFF00', '#FF0000'];
-    const carbPercent = 0.4;
-    const proteinPercent = 0.3;
-    const fatPercent = 0.3;
+    const [gramsOfProtein, setGramsOfProtein] = useState();
+    const [gramsOfCarbs, setGramsOfCarbs] = useState();
+    const [gramsOfFat, setGramsOfFat] = useState();
+    let yellow = '#FFFF00';
 
     return (
-        <div className="content">
-            <div className="row">
-                <div className="column">
-                    <GaugeChart id="protein-kpi"
-                                percent={proteinPercent}
-                                arcsLength={otherRanges}
-                                arcPadding="0.02"
-                                colors={colors} />
-                </div>
-                <div className="column">
-                    <GaugeChart id="carb-kpi"
-                                percent={carbPercent}
-                                arcsLength={carbRanges}
-                                arcPadding="0.02"
-                                colors={colors} />
-                </div>
-                <div className="column">
-                    <GaugeChart id="fat-kpi"
-                                percent={fatPercent}
-                                arcsLength={otherRanges}
-                                arcPadding="0.02"
-                                colors={colors} />
-                </div>
-            </div>
-        </div>
+        <Form className="px-5 pt-3">
+            <Row className="mb-3">
+                <Col>
+                    <h1 style={{color: yellow}}>{proteinPercentage}</h1>
+                    <Form.Group controlId="grams-of-protein">
+                        <Form.Control type="number"
+                                      placeholder="grams of protein"
+                                      value={gramsOfProtein}
+                                      onChange = {(event) => {
+                                          setGramsOfProtein(Number(event.target.value));
+                                      }}
+                        />
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Col>
+                        <h1 style={{color: yellow}}>{carbPercentage}</h1>
+                        <Form.Group controlId="grams-of-carbs">
+                            <Form.Control type="number"
+                                          placeholder="grams of carbs"
+                                          value={gramsOfCarbs}
+                                          onChange = {(event) => {
+                                              setGramsOfCarbs(Number(event.target.value));
+                                          }}
+                            />
+                        </Form.Group>
+                    </Col>
+                </Col>
+                <Col>
+                    <Col>
+                        <h1 style={{color: yellow}}>{fatPercentage}</h1>
+                        <Form.Group controlId="grams-of-fat">
+                            <Form.Control type="number"
+                                          placeholder="grams of fat"
+                                          value={gramsOfFat}
+                                          onChange = {(event) => {
+                                              setGramsOfFat(Number(event.target.value));
+                                          }}
+                            />
+                        </Form.Group>
+                    </Col>
+                </Col>
+            </Row>
+            <Button size="lg"
+                    className="m-3"
+                    onClick={() => {
+                if (allValuesHaveBeenSet(gramsOfProtein, gramsOfCarbs, gramsOfFat)) {
+                    const proteinCalories = gramsOfProtein * 4;
+                    const carbCalories = gramsOfCarbs * 4;
+                    const fatCalories = gramsOfFat * 9;
+                    const totalCalories = proteinCalories + carbCalories + fatCalories;
 
+                    setProteinPercentage(formatPercentage(proteinCalories, totalCalories));
+                    setCarbPercentage(formatPercentage(carbCalories, totalCalories));
+                    setFatPercentage(formatPercentage(fatCalories, totalCalories));
+                }
+            }}>Calculate Macros</Button>
+        </Form>
     );
+}
+
+function allValuesHaveBeenSet(gramsOfProtein, gramsOfCarbs, gramsOfFat) {
+    return gramsOfProtein !== undefined && gramsOfProtein != null &&
+        gramsOfCarbs !== undefined && gramsOfCarbs != null &&
+        gramsOfFat !== undefined && gramsOfFat != null;
+}
+
+function formatPercentage(part, total) {
+    const percentage = part / total * 100;
+    return Math.round(percentage) + '%';
 }
 
 export default KPIs;
